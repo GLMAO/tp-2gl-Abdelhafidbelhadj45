e184ff16f3efdd727b955ae0a8c4c2f42288be90
Belhadj Abdelhafid <idohaf457@gmail.com>
2025-11-20 12:50:26 +0100

    final

--- src/main/java/com/polytech/App.java
+++ src/main/java/com/polytech/App.java
@@ -1,5 +1,9 @@
 package com.polytech;
 
+import com.polytech.tp.Cours;
+import com.polytech.tp.CoursEnLigne;
+import com.polytech.tp.ICours;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        ICours cours = new Cours(
+                "Génie Logiciel",
+                "Dupont",
+                "Salle B12",
+                "20/11/2025",
+                "10:00",
+                false,
+                "L3",
+                true
+        );
+
+        cours = new CoursEnLigne(cours);
+        System.out.println(cours.getDescription());
+
+
+
     }
 }
--- src/main/java/com/polytech/tp/CoursEnLigne.java
+++ src/main/java/com/polytech/tp/CoursEnLigne.java
@@ -1,21 +1,22 @@
 package com.polytech.tp;
 
 public class CoursEnLigne extends CoursDecorator {
-    CoursEnLigne(ICours cours) {
+    public CoursEnLigne(ICours cours) {
+        super(cours);
         //TODO: Implémenter le reste ...
     }
 
     @Override
     public String getDescription() {
-        // TODO Auto-generated method stub
+        return coursDecorated.getDescription()+"(En ligne)";
         
-        return null;
+
     }
 
     @Override
     public double getDuree() {
         // TODO Auto-generated method stub
         
-        return 0;
+        return coursDecorated.getDuree();
     };
 }
